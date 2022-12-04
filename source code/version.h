25f214c2652a9d034eb1f0e37d278564deb10e60
Eril T.S. Carvalho <74083037+erilshackle@users.noreply.github.com>
2022-12-04 21:48:49 -0100

    Add files via upload

--- source code/version.h
+++ source code/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "02";
+	static const char DATE[] = "04";
 	static const char MONTH[] = "12";
 	static const char YEAR[] = "2022";
 	static const char UBUNTU_VERSION_STYLE[] =  "22.12";
@@ -17,13 +17,13 @@ namespace AutoVersion{
 	static const long MAJOR  = 2;
 	static const long MINOR  = 5;
 	static const long BUILD  = 2;
-	static const long REVISION  = 15;
+	static const long REVISION  = 17;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1012;
-	#define RC_FILEVERSION 2,5,2,15
-	#define RC_FILEVERSION_STRING "2, 5, 2, 15\0"
-	static const char FULLVERSION_STRING [] = "2.5.2.15";
+	static const long BUILDS_COUNT  = 1209;
+	#define RC_FILEVERSION 2,5,2,17
+	#define RC_FILEVERSION_STRING "2, 5, 2, 17\0"
+	static const char FULLVERSION_STRING [] = "2.5.2.17";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 4;
